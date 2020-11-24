4b931a4c4438d8bc71bafeea851c917c7a52b3f3
Franco <francopanozzo@hotmail.com.ar>
2020-11-24 13:53:42 +0100

    Added functionality to show navbar on scroll, needs to be optimized

--- scripts.js
+++ scripts.js
@@ -9,3 +9,27 @@ navToggleBtn.addEventListener('click', () => {
     header.classList.add('fixed');
   }
 });
+
+// Fucntionality to fix the navbar when scrolled
+const debounce = (fn) => {
+  let frame;
+  return (...params) => {
+    if (frame) {
+      cancelAnimationFrame(frame);
+    }
+    frame = requestAnimationFrame(() => {
+      fn(...params);
+    });
+  };
+};
+
+const handleScroll = () => {
+  console.log(window.scrollY);
+  if (window.scrollY > 50) {
+    header.classList.add('fixed');
+  } else {
+    header.classList.remove('fixed');
+  }
+};
+
+document.addEventListener('scroll', handleScroll, { passive: true });
